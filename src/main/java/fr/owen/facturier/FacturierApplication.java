47bc207347ba7b44bd0fdb72eae2e088d8f24ff5
owantdelhiver <owenboutet@gmail.com>
2023-05-17 09:15:42 +0200

    first commit

---
+++ src/main/java/fr/owen/facturier/FacturierApplication.java
@@ -0,0 +1,13 @@
+package fr.owen.facturier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacturierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/owen/facturier/FacturierApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.owen.facturier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
